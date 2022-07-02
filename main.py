64ecab32224b6a33f6ce54c5e570b7ba033b02b0
ZeeFcd <ZeeF@stud.uni-obuda.hu>
2022-07-02 21:37:34 +0200

    win function working, but it must be optimized not the best solution

--- main.py
+++ main.py
@@ -12,16 +12,27 @@ def game_board(game, player=0, row=0, column=0, just_display=False):
 
 
 def win(game):
+    # horizontal
     for row in game:
         if row[0] != 0 and row.count(row[0]) == len(row):
             print("The winner is:", row[0])
-
+    # vertical
     for i in range(len(game[0])):
         actual_column = []
         for row in game:
             actual_column.append(row[i])
         if actual_column[0] != 0 and actual_column.count(actual_column[0]) == len(actual_column):
             print("The winner is:", actual_column[0])
+    # diagonal
+    diags_left_right = []
+    diags_right_left = []
+    for i in range(len(game)):
+        diags_left_right.append(game[i][i])
+        diags_right_left.append(game[i][len(game)-1-i])
+    if diags_left_right[0] != 0 and diags_left_right.count(diags_left_right[0]) == len(diags_left_right):
+        print("The winner is:", diags_left_right[0])
+    elif diags_right_left[0] != 0 and diags_right_left.count(diags_right_left[0]) == len(diags_right_left):
+        print("The winner is:", diags_right_left[0])
 
 
 def tic_tact_toe():
@@ -29,8 +40,7 @@ def tic_tact_toe():
                 [0, 0, 0],
                 [0, 0, 0]]
     game_board(game_map, just_display=True)
-    game_board(game_map, player=2, row=1, column=0)
-    game_board(game_map, player=1, row=3, column=1)
+
     win(game_map)
 
 
