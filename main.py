b7dadd967c809d19e3e8f93d5c9df7496ac3798e
ZeeFcd <ZeeF@stud.uni-obuda.hu>
2022-07-16 01:41:35 +0200

    draw added

--- main.py
+++ main.py
@@ -11,17 +11,18 @@ def all_same(list):
         return False
 
 
-def game_board(game, player=0, row=0, column=0, just_display=False):
+def game_board_visual(game):
+    print("    " + "    ".join([str(i) for i in range(len(game))]))
+    for count, rowc in enumerate(game):
+        print(count, rowc)
+
+
+def game_board(game, player, row, column):
     try:
-        if game[row][column] != '-':
+        if game[int(row)][int(column)] != '-':
             print("This space is occupied, try another!")
             return False
-
-        print("    "+"    ".join([str(i) for i in range(len(game))]))
-        if not just_display:
-            game[row][column] = player
-        for count, rowc in enumerate(game):
-            print(count, rowc)
+        game[int(row)][int(column)] = player
         return True
     except IndexError:
         print("Did you attempt to play a row or column outside the range of 0,1 or 2?")
@@ -32,64 +33,79 @@ def game_board(game, player=0, row=0, column=0, just_display=False):
 
 
 def win(game):
-    # horizontal
-    for row in game:
-        if all_same(row):
-            print("The winner is:", row[0])
-            return True
-    # vertical
-    for i in range(len(game[0])):
-        actual_column = []
+
+    def horizontal():
         for row in game:
-            actual_column.append(row[i])
-        if all_same(actual_column):
-            print("The winner is:", actual_column[0])
+            if all_same(row):
+                return True
+
+    def vertical():
+        for i in range(len(game[0])):
+            actual_column = []
+            for row in game:
+                actual_column.append(row[i])
+            if all_same(actual_column):
+                return True
+
+    def diagonal():
+        diags_left_right = []
+        diags_right_left = []
+        for i in range(len(game)):
+            diags_left_right.append(game[i][i])
+            diags_right_left.append(game[i][len(game) - 1 - i])
+        if all_same(diags_left_right):
             return True
-    # diagonal
-    diags_left_right = []
-    diags_right_left = []
-    for i in range(len(game)):
-        diags_left_right.append(game[i][i])
-        diags_right_left.append(game[i][len(game)-1-i])
-    if all_same(diags_left_right):
-        print("The winner is:", diags_left_right[0])
+        elif all_same(diags_right_left):
+            return True
+
+    if horizontal():
         return True
-    elif all_same(diags_right_left):
-        print("The winner is:", diags_right_left[0])
+    elif vertical():
         return True
+    elif diagonal():
+        return True
+
     return False
+
+
+def game_ending():
+    again = input("The game is over, would you like to play again? (y/n) ")
+    if again.lower() == "y":
+        print("restarting!")
+        return True
+    elif again.lower() == "n":
+        print("Byeeeee!!!")
+        return False
+    else:
+        print("Not a valid answer, but... c ya!")
+        return False
 
 
 def tic_tact_toe():
     play = True
-    players = [1, 2]
     while play:
         game = [['-', '-', '-'],
                 ['-', '-', '-'],
                 ['-', '-', '-']]
-        game_won = False
+        game_ended = False
         player_cycle = itertools.cycle(['X', 'O'])
-        game_board(game, just_display=True)
-        while not game_won:
+        step_number = 0
+        while not game_ended:
             current_player = next(player_cycle)
             played = False
             while not played:
+                game_board_visual(game)
                 print(f"Player: {current_player}")
-                column = int(input("Which column? "))
-                row = int(input("Which row? "))
+                column = input("Which column? ")
+                row = input("Which row? ")
                 played = game_board(game, current_player, row, column)
-
+                step_number = step_number + 1
             if win(game):
-                game_won = True
-                again = input("The game is over, would you like to play again? (y/n) ")
-                if again.lower() == "y":
-                    print("restarting!")
-                elif again.lower() == "n":
-                    print("Byeeeee!!!")
-                    play = False
-                else:
-                    print("Not a valid answer, but... c ya!")
-                    play = False
+                game_ended = True
+                print("The winner is:", current_player)
+                play = game_ending()
+            elif step_number == 9:
+                play = game_ending()
 
 
 if __name__ == '__main__':
