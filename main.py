6c2267a216d2c37a63011bac4423629a5d548a7f
ZeeFcd <ZeeF@stud.uni-obuda.hu>
2022-07-16 15:58:51 +0200

    win function changed so that the loops inside don't make not neccesary steps

--- main.py
+++ main.py
@@ -4,11 +4,35 @@
 import itertools
 
 
-def all_same(list):
-    if list[0] != '-' and list.count(list[0]) == len(list):
-        return True
-    else:
-        return False
+def tic_tact_toe():
+    play = True
+    while play:
+        game = [['-', '-', '-'],
+                ['-', '-', '-'],
+                ['-', '-', '-']]
+        game_ended = False
+        player_cycle = itertools.cycle(['X', 'O'])
+        step_number = 0
+        while not game_ended:
+            current_player = next(player_cycle)
+            played = False
+            while not played:
+                game_board_visual(game)
+                print(f"Player: {current_player}")
+                column = input("Which column? ")
+                row = input("Which row? ")
+                played = game_board(game, current_player, row, column)
+                step_number = step_number + 1
+            if win(game):
+                game_board_visual(game)
+                game_ended = True
+                print("The winner is:", current_player)
+                play = game_ending()
+            elif step_number == 9:
+                game_board_visual(game)
+                game_ended = True
+                print("Draw!")
+                play = game_ending()
 
 
 def game_board_visual(game):
@@ -35,17 +59,23 @@ def game_board(game, player, row, column):
 def win(game):
 
     def horizontal():
-        for row in game:
-            if all_same(row):
-                return True
+        n = 0
+        while n < len(game) and not all_same(game[n]):
+            n = n + 1
+        return n < len(game)
 
     def vertical():
-        for i in range(len(game[0])):
+        n = 0
+        stop_condition = False
+        while n < len(game[0]) and not stop_condition:
             actual_column = []
             for row in game:
-                actual_column.append(row[i])
+                actual_column.append(row[n])
             if all_same(actual_column):
-                return True
+                stop_condition = True
+            else:
+                n = n + 1
+        return n < len(game[0])
 
     def diagonal():
         diags_left_right = []
@@ -57,6 +87,7 @@ def win(game):
             return True
         elif all_same(diags_right_left):
             return True
+        return False
 
     if horizontal():
         return True
@@ -66,6 +97,13 @@ def win(game):
         return True
 
     return False
+
+
+def all_same(list):
+    if list[0] != '-' and list.count(list[0]) == len(list):
+        return True
+    else:
+        return False
 
 
 def game_ending():
@@ -79,33 +117,6 @@ def game_ending():
     else:
         print("Not a valid answer, but... c ya!")
         return False
-
-
-def tic_tact_toe():
-    play = True
-    while play:
-        game = [['-', '-', '-'],
-                ['-', '-', '-'],
-                ['-', '-', '-']]
-        game_ended = False
-        player_cycle = itertools.cycle(['X', 'O'])
-        step_number = 0
-        while not game_ended:
-            current_player = next(player_cycle)
-            played = False
-            while not played:
-                game_board_visual(game)
-                print(f"Player: {current_player}")
-                column = input("Which column? ")
-                row = input("Which row? ")
-                played = game_board(game, current_player, row, column)
-                step_number = step_number + 1
-            if win(game):
-                game_ended = True
-                print("The winner is:", current_player)
-                play = game_ending()
-            elif step_number == 9:
-                play = game_ending()
 
 
 if __name__ == '__main__':
